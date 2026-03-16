a77fc475e13f86528134672eed5ffae57dbd84fc
szxzip <64679700+szxzip@users.noreply.github.com>
2026-03-16 18:36:09 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { pageConfig } from '@/uptime.config'
 
 export default function Footer() {
   const defaultFooter =
-    '<p style="text-align: center; font-size: 12px; margin-top: 10px;"> Open-source monitoring and status page powered by <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">Uptimeflare</a>, made with ❤ by <a href="https://github.com/lyc8503" target="_blank">lyc8503</a>. </p>'
+    '<p style="text-align: center; font-size: 12px; margin-top: 10px;"> Powered by <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">Uptimeflare</a>. </p>'
 
   return (
     <>
