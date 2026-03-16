6331afe36fe658e117e51b86e9b60cb74f83f98b
szxzip <64679700+szxzip@users.noreply.github.com>
2026-03-16 17:58:35 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -7,12 +7,10 @@ import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "UptimeFlare",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://szxzip.net', label: '?' },
   ],
 }
 
@@ -22,13 +20,13 @@ const workerConfig: WorkerConfig = {
     // Example HTTP Monitor
     {
       // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'foo_monitor',
+      id: '0',
       // `name` is used at status page and callback message
-      name: 'My API Monitor',
+      name: 'szxzip.net',
       // `method` should be a valid HTTP Method
       method: 'GET',
       // `target` is a valid URL
-      target: 'https://example.com',
+      target: 'https://szxzip.net',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       tooltip: 'This is a tooltip for this monitor',
       // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
