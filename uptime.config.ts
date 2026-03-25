d866a59f8fec5bdd40db1c25c4390d9ccfef38cd
szxzip <64679700+szxzip@users.noreply.github.com>
2026-03-25 20:22:25 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -3,6 +3,9 @@ import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
   title: "UptimeFlare",
+  links: [
+    { link: 'https://szxzip.net', label: 'Home', highlight: true },
+  ],
 }
 
 const workerConfig: WorkerConfig = {
